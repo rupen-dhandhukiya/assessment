03c28885b72a8300002a9bd8f00124b65af2d1df
rupen-dhandhukiya <123449651+rupen-dhandhukiya@users.noreply.github.com>
2023-01-25 16:46:09 +0530

    Add files via upload

---
+++ standard-reduced-rates-service/src/main/java/com/vat/rates/standardreduced/main/model/CountriesRates.java
@@ -0,0 +1,3 @@
+package com.vat.rates.standardreduced.main.model;
+
+public record CountriesRates (String country, double standard_rate, double reduced_rate) {}
---
+++ standard-reduced-rates-service/src/test/java/com/vat/rates/standardreduced/main/rest/controller/StandardRateRestControllerTest.java
@@ -0,0 +1,23 @@
+package com.vat.rates.standardreduced.main.rest.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StandardRateRestControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void getHighestThreeCountriesStandardRates() throws Exception {
+		mockMvc.perform(get("/standard-reduced-rates-service/vat/rate/getStandardRates")).andExpect(status().isOk());
+	}
+}
